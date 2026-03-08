485b99e8d1545686f369955e058fa88675b523ec
Yug Vithani Simform <yug.vithani@simformsolutions.com>
2026-03-09 00:30:52 +0530

    add the implicit multiplication

--- src/calculator/tokenizer.js
+++ src/calculator/tokenizer.js
@@ -5,14 +5,17 @@ export default function tokenizer(exp){
     const tokens = [];
     let i = 0, len = exp.length;
     let bracketCount = 0;
+    let lastTokenIsConstant = false;
     while(i<len){
         let currentInput = '';
+        let currIsConstant = false;
         if(isDigit(exp[i])){
             while(i<len && isDigit(exp[i])){
                 currentInput += exp[i++];
             }
         }
         else if(isConstant(exp[i])){
+            currIsConstant = true;
             currentInput += constants[exp[i++]].value
         }
         else if(isLetter(exp[i])){
@@ -21,7 +24,7 @@ export default function tokenizer(exp){
             }
         }
         else if(isOperator(exp[i])){
-            //update the no of bracket
+            //update the No of bracket
             if(exp[i] === '('){
                 bracketCount++;
             }
@@ -31,11 +34,40 @@ export default function tokenizer(exp){
 
             currentInput += operators[exp[i++]].tokenString
         }
+        //handle implicit multiplication
+        let lastToken = tokens.length > 0 ? tokens[tokens.length-1] : null;
+        let implicitMultiplication = lastToken && (
+            ([
+                operators[')'].tokenString,
+                functions['!'].tokenString,
+            ].includes(lastToken)) ||
+            !(isNaN(lastToken)) ||
+            (lastTokenIsConstant)
+        ) &&
+        (
+            ([
+                operators['('].tokenString,
+                functions['sin'].tokenString,
+                functions['cos'].tokenString,
+                functions['tan'].tokenString,
+                functions['log'].tokenString,
+                functions['ln'].tokenString,
+                functions['√'].tokenString
+            ].includes(currentInput)) ||
+            (currIsConstant)
+        );
+        if(lastToken && implicitMultiplication){
+            tokens.push('*');
+        }
+
+        lastTokenIsConstant = currIsConstant;
+
         tokens.push(currentInput);
     }
     if(bracketCount !== 0){
         showError("Mismatched brackets");
     }
+    
     return tokens;
 }
 
