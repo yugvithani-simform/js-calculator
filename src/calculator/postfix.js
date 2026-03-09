7d74433b44471a7bb35c028763221c296ba86e6f
yugvithani-simform <yug.vithani@simformsolutions.com>
2026-03-09 14:46:52 +0530

    update top and right associativity for the ^ in postfix logic

--- src/calculator/postfix.js
+++ src/calculator/postfix.js
@@ -5,7 +5,6 @@ export default function postfix(tokens){
     const stack = new Stack();
     const temp = [];
     for(let token of tokens){
-        
         if(!isNaN(token))
             stack.push(token);
         else if(token === '(')
@@ -18,8 +17,9 @@ export default function postfix(tokens){
         else{
             let curr = operators[token] ?? functions[token];
             let top = operators[temp[temp.length-1]] ?? functions[temp[temp.length-1]] ?? {precedence: -1};
-            while(temp.length && top.precedence >= curr.precedence){
+            while(temp.length && top.precedence >= curr.precedence && token!=='^'){
                 stack.push(temp.pop());
+                top = operators[temp[temp.length-1]] ?? functions[temp[temp.length-1]] ?? {precedence: -1};
             }
             temp.push(token);
         }
